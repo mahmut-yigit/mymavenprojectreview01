cf686273a354a04730913432224d7f0d911d6727
mahmut-yigit <60403058+mahmut-yigit@users.noreply.github.com>
2020-07-29 21:23:40 +0300

    Update JUnitAnnotation.java

--- src/test/java/com/techproed/JUnitAnnotation.java
+++ src/test/java/com/techproed/JUnitAnnotation.java
@@ -19,6 +19,8 @@ public class JUnitAnnotation {
     @Test
     public void test3(){
         System.out.println("Printing Test3");
+        System.out.println("GitHub icin deneme");
+        
     }
 
 }
